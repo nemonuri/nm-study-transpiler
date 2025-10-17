fa8c51d425430f79baba20486e84e227923d2b46
nemonuri <nemonuri2@gmail.com>
2025-10-17 17:03:15 +0900

    first commit

---
+++ script/New-FStarConfig.ps1
@@ -0,0 +1,25 @@
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory, Position=0)]
+    [System.IO.DirectoryInfo] $FStarRootPath
+)
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+
+if (-not $FStarRootPath.Exists) {
+    throw "Directory is not exists. ($FStarRootPath)"
+}
+
+$fstarFileName = $IsWindows ? "fstar.exe" : "fstar"
+$fstarFilePath = Join-Path $FStarRootPath bin $fstarFileName -Resolve
+$fstarLibPath = Join-Path $FStarRootPath lib fstar -Resolve
+
+$resultConfig = @{
+    fstar_exe = $fstarFilePath
+    options = @("--cache_dir", ".cache.boot", "--no_location_info", "--warn_error", "-271-272-241-319-274")
+    include_dirs = $fstarLibPath
+}
+
+ConvertTo-Json $resultConfig | Out-File "$PSScriptRoot/../.fst.config.json"
